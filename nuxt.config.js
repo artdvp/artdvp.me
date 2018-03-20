028282d74106c395c44f87d11217ea911c6a7a29
bonbonpa <sphinspf@gmail.com>
2018-03-20 20:11:45 +0700

    add : module @nuxtjs/sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
-const pkg = require('./package')
+const pkg = require("./package");
+const rt = require("./siteMapRoutes");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
   ** Headers of the page
@@ -9,45 +10,71 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
+      { hid: "keywords", name: "keywords", content: 'personal site, vue.js, nuxt.js, javascript' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', size: '32x32' },
-      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', size: '16x16' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' },
-      { rel: 'stylesheet', href: 'https://unpkg.com/reset-css@3.0.0/reset.css' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-32x32.png",
+        size: "32x32"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-16x16.png",
+        size: "16x16"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700"
+      },
+      { rel: "stylesheet", href: "https://unpkg.com/reset-css@3.0.0/reset.css" }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
 
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-    { src: '~plugins/ga.js', ssr: false }
-  ],
+  plugins: [{ src: "~plugins/ga.js", ssr: false }],
+
+  /*
+  ** Nuxt.js SiteMapRoutes
+  */
+  generate: {
+    routes: rt.routes
+  },
 
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    "bootstrap-vue/nuxt",
+    [
+      "@nuxtjs/sitemap",
+      {
+        path: "/sitemap.xml",
+        generate: true, // for build
+        routes: rt.routes
+      }
+    ]
   ],
 
   /*
@@ -68,12 +95,12 @@ module.exports = {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
---
+++ siteMapRoutes.js
@@ -0,0 +1,8 @@
+module.exports = {
+    routes: [
+        '/',
+        '/love',
+        '/js-world',
+        '/about'
+    ]
+}
